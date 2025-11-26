2e2a8edcb91e117ee2556abca9198b2812ca9c93
Diana <narduak.123@gmail.com>
2025-11-26 21:23:10 +0200

    hz

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from src.core import router as common_routes
+from src.core.router import router as common_routes
 
 app = FastAPI(
     title="Lab3 FastAPI Project",
@@ -7,4 +7,12 @@ app = FastAPI(
     version="0.1.0"
 )
 
-app.include_router(common_routes.router)
+app.include_router(common_routes)
+
+@app.get("/")
+def root():
+    return {"status": "ok", "message": "FastAPI is running!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
